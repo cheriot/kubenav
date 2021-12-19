76d22b272b041b16dfb6c30acaf4232bb2b4dcbc
Chris Heriot <cheriot@gmail.com>
2021-12-18 18:18:40 -0800

    Command line parsing stubs.

---
+++ kubenav.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	flags "github.com/jessevdk/go-flags"
+)
+
+// TODO: require kind for get command
+var globalOptions ApplicationOptions
+
+type GenCommandPositionalArgs struct {
+	Kind string `positional-arg-name:"kind" required:"true" description:"name, shortName, or category of resource(s) to query"`
+}
+
+type GetCommand struct {
+	Namespace      string                   `long:"namespace" short:"n" required:"true" description:"Namespace scope for queries"`
+	PositionalArgs GenCommandPositionalArgs `positional-args:"true"`
+}
+
+func (c *GetCommand) Execute(args []string) error {
+	fmt.Printf("Execute GetCommand %+v %+v %+v\n", globalOptions, c, args)
+	return nil
+}
+
+type ApiResourcesCommand struct{}
+
+func (c *ApiResourcesCommand) Execute(_ []string) error {
+	fmt.Printf("Execute ApiResourcesCommand\n")
+	return nil
+}
+
+type ApplicationOptions struct {
+	Verbose int `long:"verbose" short:"v"`
+}
+
+func BuildParser(appOptions *ApplicationOptions) (*flags.Parser, error) {
+	parser := flags.NewParser(appOptions, flags.Default)
+
+	getDesc := "Kind, shortName, or category of resource(s)"
+	cmd, err := parser.AddCommand("get", getDesc, getDesc, &GetCommand{})
+	if err != nil {
+		return nil, err
+	}
+	cmd.ArgsRequired = true
+
+	apiResourcesDesc := "List all API resource kinds available in the cluster."
+	_, err = parser.AddCommand("api-resources", apiResourcesDesc, apiResourcesDesc, &ApiResourcesCommand{})
+	if err != nil {
+		return nil, err
+	}
+
+	parser.CommandHandler = func(commander flags.Commander, args []string) error {
+		fmt.Printf("Set log level here. %+v\n", globalOptions)
+		return commander.Execute(args)
+	}
+
+	return parser, nil
+}
+
+func main() {
+	globalOptions = ApplicationOptions{}
+	cliParser, err := BuildParser(&globalOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// Let go-flats print errors.
+	cliParser.Parse()
+}
